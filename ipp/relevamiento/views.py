ddb0477fa400c8acf04b6312359d76554ecdfa3a
nicoechaniz <nicoechaniz@altermundi.net>
2016-02-22 03:46:56 -0300

    licenciamiento

--- ipp/relevamiento/views.py
+++ ipp/relevamiento/views.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
 
+#    IPP, Plataforma web del Índice de Precios Popular
+#    Copyright (c) 2016 Nicolás Echániz and contributors.
+#
+#    This file is part of IPP
+#
+#    IPP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from collections import OrderedDict
 
 from django.http import Http404
